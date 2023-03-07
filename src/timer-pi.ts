dd4bb17247d6aef90286cfe05ecb040be3ee5691
Emma Litwa-Vulcu <emma@heysparkbox.com>
2023-03-07 10:52:35 -0800

    feat: set task options from harvest data

--- src/timer-pi.ts
+++ src/timer-pi.ts
@@ -2,12 +2,16 @@
 /// <reference path="libs/js/utils.js" />
 
 import config from '../config.js';
+import { getUserProjectAssignments } from './api/harvest';
+import { ProjectAssignment, Settings, TaskAssignment } from './types';
 
 $PI.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }): void => {
   const { payload, context } = actionInfo;
   const form = document.getElementById('property-inspector');
 
-  Utils.setFormValue(payload.settings, form);
+  setTaskOptions(payload.settings).then(() => {
+    Utils.setFormValue(payload.settings, form);
+  });
 
   form.addEventListener(
     'input',
@@ -36,3 +40,30 @@ $PI.onDidReceiveSettings(config.appName + '.timer', ({ payload }): void => {
 document.getElementById('howto-button').addEventListener('click', () => {
   window.open('https://github.com/reiniiriarios/stream-deck-billables-harvest/#readme');
 });
+
+/**
+ * Set <select> options for task selection.
+ *
+ * @param {Settings} settings
+ */
+const setTaskOptions = async (settings: Settings) => {
+  let select = document.getElementById('task');
+  const projectAssignments = await getUserProjectAssignments(settings);
+  projectAssignments.forEach((project_assignment: ProjectAssignment) => {
+    // Project optgroup
+    let optgroup = document.createElement('optgroup');
+    optgroup.label = project_assignment.project.name;
+    project_assignment.task_assignments.forEach((task_assignment: TaskAssignment) => {
+      // Task assignment option
+      let option = document.createElement('option');
+      option.text = task_assignment.task.name;
+      option.value = JSON.stringify({
+        project: project_assignment.project.id,
+        task_assignment: task_assignment.id,
+        task: task_assignment.task.id,
+      });
+      optgroup.appendChild(option);
+    });
+    select.appendChild(optgroup);
+  });
+};
