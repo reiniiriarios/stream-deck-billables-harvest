d2de6a1b5f2b4a44175c3035c2f4d60c151f8346
Emma Litwa-Vulcu <emma@heysparkbox.com>
2023-03-08 13:26:10 -0800

    feat: success and empty (no assigned hours) icons

--- src/display.ts
+++ src/display.ts
@@ -27,59 +27,38 @@ const formatTimer = (hours: number): string => {
 };
 
 /**
- * The icon svg data.
+ * Icon config.
+ */
+const CANVAS_SIZE = 144;
+const TEXT_COLOR = '#fff';
+const BRAND_COLOR = '#fa5c00';
+const OVERAGE_COLOR = '#f00';
+const EMPTY_COLOR = '#333';
+const FONT_SIZE = 32;
+
+/**
+ * Icon svg data.
  *
  * Easy editor: https://yqnn.github.io/svg-path-editor/
  */
 const ICONS = {
-  continue: {
-    path: 'M 38 18 C 38 13 47 13 47 18 v 64 C 47 87 38 87 38 82 V 20 z m 31 -3 C 66 13 62 14 62 19 v 62 c 0 5 4 5 7 3 l 44 -30 C 116 52 116 48 113 46 l -44 -31 z M 102 50 l -31 22 V 28 L 102 50 z',
-    color: '#0e74e8',
-  },
-  stop: {
-    path: 'M 53 12 H 60 a 4 4 90 0 1 4 4 V 77 a 4 4 90 0 1 -4 4 H 53 a 4 4 90 0 1 -4 -4 V 16 A 4 4 90 0 1 53 12 Z m 31 69 h 7 a 4 4 90 0 0 4 -4 V 16 a 4 4 90 0 0 -4 -4 H 84 a 4 4 90 0 0 -4 4 V 77 A 4 4 90 0 0 84 81 Z',
-    color: '#fa5c00',
-  },
-  stopReally: {
-    path: 'M 44 58 a 7 7 90 0 0 7 7 h 41 a 7 7 90 0 0 7 -7 V 16 a 7 7 90 0 0 -7 -7 H 51 a 7 7 90 0 0 -7 7 v 41 z',
-    color: '#e02222',
-  },
   success: {
     path: 'M 114 19 L 65 68 L 39 42 L 31 50 l 34 34 l 57 -57 z',
     color: '#0cc952',
   },
   timerRunning: {
     path: 'M 60.4 39 C 58 37.4 54.8 38.2 54.8 42.2 v 49.6 c 0 4 3.2 4 5.6 2.4 l 35.2 -24 C 98 68.6 98 65.4 95.6 63.8 l -35.2 -24.8 z M 86.8 67 l -24.8 17.6 V 49.4 L 86.8 67 z',
-    color: '#fa5c00',
+    color: BRAND_COLOR,
   },
   timerStopped: {
     path: 'M 57.58 40.52 H 62.9 a 3.04 3.04 90 0 1 3.04 3.04 V 89.92 a 3.04 3.04 90 0 1 -3.04 3.04 H 57.58 a 3.04 3.04 90 0 1 -3.04 -3.04 V 43.56 A 3.04 3.04 90 0 1 57.58 40.52 Z m 23.56 52.44 h 5.32 a 3.04 3.04 90 0 0 3.04 -3.04 V 43.56 a 3.04 3.04 90 0 0 -3.04 -3.04 H 81.14 a 3.04 3.04 90 0 0 -3.04 3.04 V 89.92 A 3.04 3.04 90 0 0 81.14 92.96 Z',
     color: '#575757',
   },
+  empty: {
+    path: 'M 72 82.5 A 1.875 1.875 90 0 0 72 15 A 1.875 1.875 90 0 0 72 82.5 M 72 75 A 1.875 1.875 90 0 1 72 22.5 A 1.875 1.875 90 0 1 72 75 Z M 100.125 15 L 38.25 76.875 L 43.875 82.5 L 105.75 20.625 Z',
+    color: EMPTY_COLOR,
+  },
 };
-
-/**
- * Get the icon to display based on hours remaining.
- *
- * @param {number} hours
- * @returns {{ path: string; color: string }} icon data
- */
-const getStatusIcon = (hours: number): { path: string; color: string } => {
-  if (hours < 0.5) {
-    return ICONS.stopReally;
-  }
-  if (hours < 0.25) {
-    return ICONS.stop;
-  }
-  if (hours > 0.25) {
-    return ICONS.continue;
-  }
-  return ICONS.success;
-};
-
-const CANVAS_SIZE = 144;
-const TEXT_COLOR = '#fff';
-const FONT_SIZE = 32;
 
 /**
  * Display the hours remaining.
@@ -102,29 +81,30 @@ export const displayHoursRemaining = (
 
   // Icon
   if (!assignedHours) {
-    // @todo draw something else?
-  }
-  else {
+    const path = new Path2D(ICONS.empty.path);
+    ctx.fillStyle = ICONS.empty.color;
+    ctx.fill(path);
+  } else {
     let percentageRemaining = assignedHours - hoursRemaining / assignedHours;
     // check success with an error margin
     if (percentageRemaining <= 0.01 && percentageRemaining >= -0.01) {
-      // @todo draw checkbox
-    }
-    else {
+      const path = new Path2D(ICONS.success.path);
+      ctx.fillStyle = ICONS.success.color;
+      ctx.fill(path);
+    } else {
       // draw pie chart
       let workedHours: number;
       let filledColor: string, emptyColor: string;
       if (hoursRemaining < 0) {
         // overage, we worked too long today
-        filledColor = '#f00';
-        emptyColor = '#fa5c00';
+        filledColor = OVERAGE_COLOR;
+        emptyColor = BRAND_COLOR;
         // hours worked should display as the amount over we are
         workedHours = hoursRemaining * -1;
-      }
-      else {
+      } else {
         // under, there are hours remaining to be worked
-        filledColor = '#fa5c00';
-        emptyColor = '#333';
+        filledColor = BRAND_COLOR;
+        emptyColor = EMPTY_COLOR;
         workedHours = assignedHours - hoursRemaining;
       }
       const centerX = CANVAS_SIZE * 0.5;
@@ -148,7 +128,14 @@ export const displayHoursRemaining = (
       const offsetY = Math.sin(angleCenter) * offset;
       ctx.beginPath();
       ctx.moveTo(centerX + offsetX, centerY + offsetY);
-      ctx.arc(centerX + offsetX, centerY + offsetY, radius * 0.75, startAngle + workedHoursAngle, endAngle, false);
+      ctx.arc(
+        centerX + offsetX,
+        centerY + offsetY,
+        radius * 0.75,
+        startAngle + workedHoursAngle,
+        endAngle,
+        false
+      );
       ctx.lineTo(centerX + offsetX, centerY + offsetY);
       ctx.fillStyle = emptyColor;
       ctx.fill();
