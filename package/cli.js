57a4cfd4c6de77d4289123a92aec216a5b669505
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-07-14 16:15:36 -0700

    chore: add distribution tool locally
    elgato has changed how the files can be accessed, so these must be stored here now

---
+++ package/cli.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
+const config = require("../config");
+
+const tool = process.platform === "win32" ? "DistributionTool.exe" : "DistributionTool";
+const toolPath = path.join(__dirname, tool);
+
+const buildDir = `build/${config.appName}.sdPlugin`;
+const packageFile = `build/${config.appName}.streamDeckPlugin`;
+
+if (fs.existsSync(packageFile)) {
+  fs.unlinkSync(packageFile);
+}
+
+spawn(toolPath, ['-b', '-i', buildDir, '-o', 'build'], { stdio: "inherit" }).on("exit", process.exit);
